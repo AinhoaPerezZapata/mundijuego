522dc87b6c95338c8a4d4542a3382492290b6ec2
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-09 19:43:57 +0100

    Update desloguear_usuario.php

--- Codigo/final/php/desloguear_usuario.php
+++ Codigo/final/php/desloguear_usuario.php
@@ -1,5 +1,4 @@
 <?php
-            //INICIA LA SESION Y LUEGO LA DESTRUYE(DESLOGUANDOSE)
                 session_start();
                 session_destroy();
                 header('Location: ../Login.html');
