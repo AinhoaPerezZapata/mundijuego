d3064e5b8253a01e90e5c2d8f84afa668d6a0e06
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-09 21:56:58 +0100

    Update panel.php

--- Codigo/php/panel.php
+++ Codigo/php/panel.php
@@ -38,17 +38,17 @@
                 <button type ="submit"><a href="../insertarusuario.html?idUsuario=<?php echo $fila['idUsuario']; ?>" value="insertar" name="insertar">Insertar Usuario</button>
             </tr>
 
-
+                <hr>dddddddddddddd</hr>
             <?php
                 //Cargamos los archivos que vamos a usar
                 require "BD/conectorBD.php";
                 require "BD/DAOUsuarios.php";
 
                 //Nos conectamos a la base de datos y mostramos en este caso los usuarios
-
+echo 'ddddddddddddddsdsdsd';
                 $conexion = conectar(false);
                 $result = mostrarUsuario($conexion);
-
+echo 'ddddddddddddddsdsddfsdfsdfsfsdfsdfsdsd';
                     while($fila=mysqli_fetch_assoc($result)){
                 ?>
 
