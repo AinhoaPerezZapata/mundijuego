047a2302ec2274d35b42deddf18273394fa9b466
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-09 21:11:16 +0100

    Update panel.php

--- Codigo/php/panel.php
+++ Codigo/php/panel.php
@@ -69,6 +69,7 @@ session_start();
                         <?php  
                                 }
                         ?>
+            <hr>aaaaaaaaaaaaaaaaaa</hr>
         </table>
         </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
