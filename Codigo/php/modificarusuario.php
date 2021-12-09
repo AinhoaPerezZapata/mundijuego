e6641d4722f62f54e140224661dcad18383be44f
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-09 20:32:28 +0100

    Add files via upload

---
+++ Codigo/php/modificarusuario.php
@@ -0,0 +1,49 @@
+<?php
+//Cargamos los archivos que vamos a usar
+require "BD/DAOusuarios.php";
+require "BD/conectorBD.php";
+//Nos conectamos a la base de datos
+$conexion = conectar(false);
+//Obtenemos el apartado que queremos utilizar.
+$idUsuario = $_GET['idUsuario'];
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../styles/estilos.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,700i|Open+Sans:400,700&display=swap" rel="stylesheet"> 
+        <title>Modificar Usuario</title>
+    </head>
+<body>
+    <nav class="nav-main1">
+    <div align="right"><img src="../img/logo/logo.png"></div>
+        <a href="../php/index.php">Inicio</a>
+        <a href="videojuegos.php">Videojuegos</a>
+        <a id="link" class="nav-link active" aria-current="page" href="../login.html">Login</a>
+        <a id="link" class="nav-link" href="../ingresar_usuario.html">Registro</a>
+        <a href="../mapasitio.html">Mapa del sitio</a>
+    </nav>  
+    <br>
+    
+    <div class="contenedor"> 
+    <form action="actualizarusuario.php" method="POST" enctype="multipart/form-data">
+    <center>Rol:<select name="Rol"></center>
+        <option value="usuario">Usuario</option>
+        <option value="admin">Admin</option>
+        </select><br>
+        <br>
+       <input type="submit" value="Actualizar" name="btnregistrar">
+       <input type="hidden"  name="idUsuario" value="<?php echo $idUsuario ?>">
+    </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="http://1000hz.github.io/bootstrap-validator/dist/validator.min.js"></script>
+</body>
+</html>
---
+++ Codigo/php/panel.php
@@ -0,0 +1,77 @@
+<?php
+//Cargamos los archivos que vamos a usar
+require "BD/DAOusuarios.php";
+require "BD/conectorBD.php";
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles/estilos.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700,700i|Open+Sans:400,700&display=swap" rel="stylesheet"> 
+  
+    <title>Panel de Usuarios</title>
+</head>
+    <body>
+        <header>
+        <nav class="nav-main1">
+        <div align="right"><img src="../img/logo/logo.png"></div>
+            <a href="../php/index.php">Inicio</a>
+            </nav>   
+              <ul class="nav justify-content-end"></ul>
+        </header>  
+        <div class="contenedor">
+        <table border="1" class="table table-dark">
+            <tr>
+                <th>idUsuario</th>
+                <th>Usuario</th>
+                <th>Password</th>
+                <th>Nombre</th>
+                <th>Apellido1</th>
+                <th>Apellido2</th>
+                <th>Telefono</th>
+                <th>Email</th>
+                <th>CP</th>
+                <th>Provincia</th>
+                <th>ComunidadAutonoma</th>
+                <th>Rol</th>
+                <button type ="submit"><a href="../insertarUsuario.html?idUsuario=<?php echo $fila['idUsuario']; ?>" value="insertar" name="insertar">Insertar Usuario</button>
+            </tr>
+
+            <?php
+                //Nos conectamos a la base de datos y mostramos en este caso los usuarios
+
+                $conexion = conectar(false);
+                $result = mostrarUsuario($conexion);
+
+                    while($fila=mysqli_fetch_assoc($result)){?>
+
+                <tr>
+                        <?php 
+                        
+                            foreach($fila as $key => $value){?>
+                                <td> <?= $value ?> </td>
+                                <?php
+                                    }
+                                ?>
+
+                <td><button ><a href="modificarusuario.php?idUsuario= <?php echo $fila['idUsuario'];?>" value="modificar" name="modificar">Modificar</button></td>
+                <td><button ><a href="eliminarUsuario.php?idUsuario= <?php echo $fila['idUsuario'];?>" value="eliminar" name="eliminar">Eliminar</button></td>
+                    
+                </tr>                
+                        <?php  
+                                }
+                        ?>
+        </table>
+        </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+            <script src="http://1000hz.github.io/bootstrap-validator/dist/validator.min.js"></script>
+    </body>
+</html>
