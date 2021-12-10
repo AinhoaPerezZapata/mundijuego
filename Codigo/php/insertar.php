ec02e44a0c6f19f0d51c18bd5d36fad56627c7d8
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-10 20:16:38 +0100

    Update insertar.php

--- Codigo/php/insertar.php
+++ Codigo/php/insertar.php
@@ -8,7 +8,6 @@ $Nombre = $_POST["Nombre"];
 $ImagenesN=$_FILES['Imagen']['name'];
 $imagenA=$_FILES['Imagen']['tmp_name'];
 $rutaImg="../img/".$ImagenesN;
-move_uploaded_file($imagenA,$rutaImg);
 
 //Nos conectamos a la base de datos y a la consulta
 $conexion = conectar(false);
