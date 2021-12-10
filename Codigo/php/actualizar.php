1dedba0f1e3e88a6ef029286a602e8b1b416f9b5
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-10 20:19:19 +0100

    Update actualizar.php

--- Codigo/php/actualizar.php
+++ Codigo/php/actualizar.php
@@ -17,7 +17,6 @@ $Nombre = $_POST["Plataforma"];
 $ImagenesN=$_FILES['Imagen']['name'];
 $imagenA=$_FILES['Imagen']['tmp_name'];
 $rutaImg="../img/".$ImagenesN;
-$idPlataforma = $_POST['Idplataforma'];
 
 move_uploaded_file($imagenA,$rutaImg);
 if(empty($_FILES['Imagen']['name'])){
