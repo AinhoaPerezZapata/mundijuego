5e53bb7199a6cbb1b2aff22abe1cd865616ea8c4
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-10 23:56:16 +0100

    Update DAOUsuarios.php

--- Codigo/php/BD/DAOUsuarios.php
+++ Codigo/php/BD/DAOUsuarios.php
@@ -49,8 +49,8 @@
             $resultado = mysqli_query($conexion, $consulta);
             return $resultado;
         }
-        function insertarUsuario($conexion,$usuario,$password,$email,$DNi,$Apellido1,$Apellido2,$ComunidadAutonoma,$Nombre,$Telefono,$CP,$Provincia,$FechaNacimiento,$Rol){
-            $consulta = "INSERT INTO usuario ( `Usuario`, `Password`, `Nombre`, `Apellido1`, `Apellido2`, `Telefono`, `Email`, `CP`, `Provincia`, `ComunidadAutonoma`,`Direccion`, `Rol`,`DNi`,`FechaNacimiento`) VALUES ('$usuario', '$password', '$Nombre', '$Apellido1', '$Apellido2', '$Telefono', '$email', '$CP', '$Provincia', '$ComunidadAutonoma','$Direccion', '$Rol','$DNi', '$FechaNacimiento')";
+        function insertarUsuario($conexion,$Usuario,$Password,$Email,$DNi,$Apellido1,$Apellido2,$ComunidadAutonoma,$Nombre,$Telefono,$CP, $Direccion, $Provincia, $FechaNacimiento,$Rol){
+            $consulta = "INSERT INTO usuario ( `Usuario`, `Password`, `Nombre`, `Apellido1`, `Apellido2`, `Telefono`, `Email`, `CP`, `Provincia`, `ComunidadAutonoma`,`Direccion`, `Rol`,`DNi`,`FechaNacimiento`) VALUES ('$Usuario', '$Password', '$Nombre', '$Apellido1', '$Apellido2', '$Telefono', '$Email', '$CP', '$Provincia', '$ComunidadAutonoma','$Direccion', '$Rol','$DNi', '$FechaNacimiento')";
 
             $resultado = mysqli_query($conexion, $consulta);
             return $resultado;
