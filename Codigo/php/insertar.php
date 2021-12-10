668125db6ca0a342f313980ec5e926eda18db950
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-10 20:22:49 +0100

    Update insertar.php

--- Codigo/php/insertar.php
+++ Codigo/php/insertar.php
@@ -1,14 +1,20 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+<?php
 //Cargamos los archivos que vamos a usar
 
 require "BD/DAOplataforma.php";
 require "BD/conectorBD.php";
 //Usamos las variables que vamos a coger
-$Nombre = $_POST["Nombre"];
+$Nombre = $_POST["Plataforma"];
 $ImagenesN=$_FILES['Imagen']['name'];
 $imagenA=$_FILES['Imagen']['tmp_name'];
 $rutaImg="../img/".$ImagenesN;
-move_uploaded_file($imagenA,$rutaImg);
+$idPlataforma = $_POST['Idplataforma'];
+
 //Nos conectamos a la base de datos y a la consulta
 $conexion = conectar(false);
 $consulta = insertarProductos($conexion, $Nombre, $rutaImg);
