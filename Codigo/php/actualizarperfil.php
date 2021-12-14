eb27af498fc9eeaee188f8152e389f288de12dd6
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-14 23:25:01 +0100

    Update actualizarperfil.php

--- Codigo/php/actualizarperfil.php
+++ Codigo/php/actualizarperfil.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 //Cargamos los archivos que vamos a usar
 
 require 'BD/DAOUsuarios.php';
