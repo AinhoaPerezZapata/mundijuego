4c8f1dc0dc1b74aa4dc493d3b4c3b24e7df1a451
AinhoaPerezZapata <91693066+AinhoaPerezZapata@users.noreply.github.com>
2021-12-14 03:44:24 +0100

    Update DAONoticia.php

--- Codigo/php/BD/DAONoticia.php
+++ Codigo/php/BD/DAONoticia.php
@@ -5,7 +5,7 @@
             return $resultado;
         }
         function buscador($conexion, $noticia){
-            $consulta = "SELECT * FROM noticias WHERE NombreP  LIKE '%$noticia%'";
+            $consulta = "SELECT * FROM noticias WHERE Titulo LIKE '%$noticia%'";
             $resultado = mysqli_query($conexion, $consulta);
             return $resultado;
         }
